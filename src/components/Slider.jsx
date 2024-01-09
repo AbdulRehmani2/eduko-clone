9afb468963988691b911b4a9b74b4249a70472da
AbdulRehmani2 <abdulrehmanirfan2238@gmail.com>
2024-01-09 22:42:14 +0500

    Slider Complete

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -11,40 +11,39 @@ export default function Slider()
     function nextPage(){
         index < 2 ? setIndex(index+1) : setIndex(0)
     }
-
+    
     function prevPage(){
         index > 0 ? setIndex(index-1) : setIndex(2)
     }
 
-    let data = [{title:"Available on Every Device", firstLine: 'Eduko is available on Mobile and Desktop Devices', secondLine:'The most advanced system to make the course presence on the Web and Mobile devices.', image:slide1},
-                {title:'Online Assignment Submission', firstLine:'Forget the paper work', secondLine:'The integrated API helps the students to manage their assignments online', image:slide2},
-                {title:'Best Platform to Manage the Course', firstLine:'Manage your classes with online presence', secondLine:'Create your own course and make it more productive', image:slide3}]
+    let data = [{title:"Available on Every Device", firstLine: 'Eduko is available on Mobile and Desktop Devices', secondLine:'The most advanced system to make the course presence on the Web and Mobile devices.', image:slide1, index:0},
+                {title:'Online Assignment Submission', firstLine:'Forget the paper work', secondLine:'The integrated API helps the students to manage their assignments online', image:slide2, index:1},
+                {title:'Best Platform to Manage the Course', firstLine:'Manage your classes with online presence', secondLine:'Create your own course and make it more productive', image:slide3, index:2}]
     return (
-        <div className="slider-container" style={{backgroundImage: `url(${data[index].image})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', filter:'brightness(90%)'}}>
-            <button className="slider-buttons" onClick={nextPage}>&lt;</button>
+        <div className="slider-container" style={{backgroundImage: `url(${data[index].image})`, backgroundSize:'cover', backgroundRepeat:'no-repeat'}}>
+            <button className="slider-buttons" onClick={prevPage}>&lt;</button>
             <Slide data={data[index]}></Slide>
-            <button className="slider-buttons" onClick={prevPage}>&gt;</button>
+            <button className="slider-buttons" onClick={nextPage}>&gt;</button>
         </div>
     )
-
+    
     function Slide(props)
     {
         function changeSlide(e){
-            let slideNo = Number(e.target.classList.value)
+            let slideNo = Number(e.target.id)
             setIndex(slideNo)
-            let elements = document.querySelectorAll('.slider-controls>li')
-            e.target.style.backgroundColor = 'white'
         }
 
         return (<>
+            {}
             <div className="slide-container">
                 <h1>{props.data.title}</h1>
                 <p>{props.data.firstLine}</p>
                 <p>{props.data.secondLine}</p>
                 <ul className="slider-controls">
-                    <li className="0" onClick={changeSlide}></li>
-                    <li className="1" onClick={changeSlide}></li>
-                    <li className="2" onClick={changeSlide}></li>
+                    <li className={props.data.index == 0 ? 'active' : 'notActive'} id="0" onClick={changeSlide}></li>
+                    <li className={props.data.index == 1 ? 'active' : 'notActive'} id="1" onClick={changeSlide}></li>
+                    <li className={props.data.index == 2 ? 'active' : 'notActive'} id="2" onClick={changeSlide}></li>
                 </ul>
             </div>
         </>)
