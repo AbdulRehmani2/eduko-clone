68e0804499ef52372c12f3614cd75b03b971d09f
AbdulRehmani2 <abdulrehmanirfan2238@gmail.com>
2024-01-09 23:30:36 +0500

    Card component complete

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,9 +3,16 @@ import image from '../assets/course.jpg'
 
 export default function Cards()
 {
+    let data = [{title:'Artificial Intelligence', teacher:'Samyan Qayyum Wahla', year:'Fall 2023', description:'This course introduces representations, techniques, and architectures used to build applied...'},
+                {title:'Data Structures and Algorithms Lab (CS A,B)', teacher:'Nazeef Ul Haq', year:'Fall 2023', description:'...'}, 
+                {title:'Data Structures and Algorithms', teacher:'Nazeef Ul Haq', year:'Fall 2023', description:'The course introduces the theory of complexity and abstract data structures as basic building..'},
+                {title:'Final Year Project(2020)', teacher:'Samyan Qayyum Wahla', year:'Spring 2023', description:'The main objective of the final year project is to provide students with a design experience in...'}]
     return (
         <div className="cards-container">
-            <Card></Card>
+            <Card data={data[0]}></Card>
+            <Card data={data[3]}></Card>
+            <Card data={data[2]}></Card>
+            <Card data={data[1]}></Card>
         </div>
     )
 }
@@ -16,11 +23,11 @@ function Card(props)
         <div className="card-container">
             <img src={image}></img>
             <div className="card-text-container">
-                <h2>Artificial Intelligence</h2>
-                <h4>Nazeef</h4>
-                <h4>2022</h4>
-                <p>Hello this is Nazeef</p>
-                <button>Read More</button>
+                <h3>{props.data.title}</h3>
+                <h4>{props.data.teacher}</h4>
+                <h4>{props.data.year}</h4>
+                <p>{props.data.description}</p>
+                <button>READ MORE</button>
             </div>
         </div>
     )
